d6fd94b2537a10bb3d392b5d61dc54751491b501
TRTL <4seven@gmail.com>
2023-01-05 21:22:01 +0200

    added address to user

--- PROJECT-CA23/PROJECT-CA23/Models/User.cs
+++ PROJECT-CA23/PROJECT-CA23/Models/User.cs
@@ -9,19 +9,33 @@ namespace PROJECT_CA23.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserId { get; set; }
+
         [MaxLength(100)]
         public string Username { get; set; }
+
         [MaxLength(200)]
         public string FirstName { get; set; }
+
         [MaxLength(200)]
         public string LastName { get; set; }
-        [MaxLength(100)]
+
         public ERole Role { get; set; }
+
         public byte[] PasswordHash { get; set; }
+
         public byte[] PasswordSalt { get; set; }
+
         public DateTime Created { get; set; }
+
         public DateTime Updated { get; set; }
+
         public DateTime LastLogin { get; set; }
+
         public bool IsDeleted { get; set; }
+
+        [ForeignKey("Address")]
+        public int AddressId { get; set; }
+
+        public Address Address { get; set; }
     }
 }
