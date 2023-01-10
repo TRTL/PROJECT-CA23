0460bc3beddc5a0ea041579122a566c9d31b2a44
TRTL <4seven@gmail.com>
2023-01-10 21:57:44 +0200

    Data annotations for model validation added.

--- PROJECT-CA23/PROJECT-CA23/Models/Notification.cs
+++ PROJECT-CA23/PROJECT-CA23/Models/Notification.cs
@@ -1,12 +1,24 @@
-﻿namespace PROJECT_CA23.Models
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PROJECT_CA23.Models
 {
     public class Notification
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int NotificationId { get; set; }
+
+        [Required(ErrorMessage = "UserId is required")]
         public int UserId { get; set; }
         public virtual User User { get; set; }
+
+        [MaxLength(100, ErrorMessage = "Notification title text cannot be longer than 100 symbols")]
         public int Title { get; set; }
+
+        [MaxLength(1000, ErrorMessage = "Notification text cannot be longer than 1000 symbols")]
         public int Text { get; set; }
-        public bool Shown { get; set; }
+        public bool Shown { get; set; } = false;
     }
 }
--- PROJECT-CA23/PROJECT-CA23/Models/Review.cs
+++ PROJECT-CA23/PROJECT-CA23/Models/Review.cs
@@ -1,9 +1,13 @@
 ﻿using PROJECT_CA23.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PROJECT_CA23.Models
 {
     public class Review
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserId { get; set; }
         public virtual User User { get; set; }
         public int MediaId { get; set; }
