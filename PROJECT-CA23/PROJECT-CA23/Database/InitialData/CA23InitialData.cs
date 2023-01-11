161d64c2f3a76dac79a4879aff3601a2456201e6
TRTL <4seven@gmail.com>
2023-01-11 21:33:21 +0200

    initialData file created

---
+++ PROJECT-CA23/PROJECT-CA23/Database/InitialData/CA23InitialData.cs
@@ -0,0 +1,29 @@
+﻿using PROJECT_CA23.Models;
+using PROJECT_CA23.Models.Enums;
+using PROJECT_CA23.Services;
+using PROJECT_CA23.Services.IServices;
+
+namespace PROJECT_CA23.Database.InitialData
+{
+    public class CA23InitialData
+    {
+
+
+        public static readonly User[] userInidialData = new User[]
+        {
+
+            new User()
+            {
+                UserId = 1,
+                Username = "admin",
+                FirstName = "Jonas",
+                LastName = "Jonaitis",
+                Role = ERole.admin,
+                //PasswordHash= adminPasswordHash,
+            }
+        };
+
+
+    }
+    
+}
