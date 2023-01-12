b571d44182fb741579c6b64e223a5abbb9294ed0
TRTL <4seven@gmail.com>
2023-01-12 19:18:40 +0200

    AddressDto refactored

--- PROJECT-CA23/PROJECT-CA23/Models/Dto/AddressDtos/AddressDto.cs
+++ PROJECT-CA23/PROJECT-CA23/Models/Dto/AddressDtos/AddressDto.cs
@@ -6,7 +6,9 @@ namespace PROJECT_CA23.Models.Dto.AddressDto
     public class AddressDto
     {
         public int UserId { get; set; }
-        public UserDto User { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
         public string Country { get; set; }
         public string City { get; set; }
         public string AddressText { get; set; }
