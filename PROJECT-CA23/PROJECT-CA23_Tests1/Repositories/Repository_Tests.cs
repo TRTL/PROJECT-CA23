4dd26ddeaf08760da14802a3fd5d6443a5b7defc
TRTL <4seven@gmail.com>
2023-01-16 16:31:39 +0200

    Context test created. Inital data seed fixed.

---
+++ PROJECT-CA23/PROJECT-CA23_Tests1/Repositories/Repository_Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PROJECT_CA23.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJECT_CA23.Repositories_Tests
+{
+    [TestClass()]
+    public class Repository_Tests
+    {
+        [TestMethod()]
+        public void CreateAsync_Test()
+        {
+            Assert.Fail();
+        }
+    }
+}
